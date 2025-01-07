f87a4b4e49123dc5d694b625c95edb235377d9a3
Saurabh Kumar <saurabh.k@ipsator.com>
2025-01-07 16:59:38 +0530

    config setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,8 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {};
-
-// export default nextConfig;
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    basePath: '/real-estate-ui', // Replace with your repository name
-    assetPrefix: '/real-estate-ui', // Replace with your repository name
-  };
-  
-  export default nextConfig;
+const nextConfig = {};
+
+export default nextConfig;
+
+
+
   
